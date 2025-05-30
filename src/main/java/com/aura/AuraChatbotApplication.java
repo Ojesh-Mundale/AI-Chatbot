398425f9dad7de1da494f5d1a247d30dc2a3494f
Ojesh <ojeshmundale@gmail.com>
2025-05-30 14:31:25 +0530

    Initial commit

---
+++ src/main/java/com/aura/AuraChatbotApplication.java
@@ -0,0 +1,12 @@
+package com.aura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraChatbotApplication.class, args);
+    }
+}
